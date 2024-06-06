b913ea3475419aabbcb989892e5c8863eb420cc3
David Rahmadana <110660092+medavidrahmadana@users.noreply.github.com>
2024-06-07 00:22:38 +0700

    Update Calculator.java
    .

--- src/main/java/praktek_mkpl/Calculator.java
+++ src/main/java/praktek_mkpl/Calculator.java
@@ -15,7 +15,7 @@ public class Calculator {
 
     public int divide(int a, int b) {
         if (b == 0) {
-            throw new IllegalArgumentException("Division by zeroo");
+            throw new IllegalArgumentException("Division by zero");
         }
         return a / b;
     }
